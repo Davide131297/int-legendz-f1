a4846d3b599c9d8bce400c6a649d3ccde16425ce
davidechiffi <davide.chiffi@iu-study.org>
2024-05-28 00:21:07 +0200

    Archiv

--- src/Archiv/Archiv.js
+++ src/Archiv/Archiv.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, Image, Text, Badge, Button, Group, Title , Modal} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import './Archiv.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+import Constructor_23 from './Constructor_23.jpeg';
+import Driver_23 from './Driver_23.jpeg';
+import Statistics from './Statistics_23.jpeg';
 
 const Archiv = () => {
 
     const [opened, { open, close }] = useDisclosure(false);
+
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
 
     return (
         <>
@@ -23,7 +33,7 @@ const Archiv = () => {
 
                     <Group justify="space-between" mt="md" mb="xs">
                         <Text fw={500}>F1 23 Aufwärm Saison </Text>
-                        <Badge color="red">Läuft Aktuell</Badge>
+                        <Badge color="red">Beendet am 24.05.2024</Badge>
                     </Group>
 
                     <Text size="sm" c="dimmed">
@@ -36,8 +46,30 @@ const Archiv = () => {
                 </Card>
             </div>
 
-            <Modal opened={opened} onClose={close} title="F1 23 Aufwärm Saison" centered>
-                Saison ist noch nicht beendet...
+            <Modal opened={opened} onClose={close} title="F1 23 Aufwärm Saison" centered size="90rem">
+                <Carousel activeIndex={index} onSelect={handleSelect} fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={Constructor_23}
+                            alt="Constructor 23"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={Driver_23}
+                            alt="Driver 23"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={Statistics}
+                            alt="Statistics"
+                        />
+                    </Carousel.Item>
+                </Carousel>
             </Modal>
 
         </>
