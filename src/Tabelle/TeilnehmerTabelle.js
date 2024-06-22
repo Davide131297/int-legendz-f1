bae206ccbb712c68651a7f723a1858493fa355e6
davidechiffi <davide.chiffi@iu-study.org>
2024-06-22 21:36:01 +0200

    Teilnehmertabelle bekommt jetzt die Renndaten in der Flagge

--- src/Tabelle/TeilnehmerTabelle.js
+++ src/Tabelle/TeilnehmerTabelle.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { db } from './../utils/firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, getDoc, doc } from 'firebase/firestore';
 import Table from 'react-bootstrap/Table';
 import './TeilnehmerTabelle.css';
-import { ScrollArea } from '@mantine/core';
+import { ScrollArea, Box } from '@mantine/core';
 import {useNavigate} from 'react-router-dom';
-import { getStorage, ref, listAll, getDownloadURL} from 'firebase/storage';
-import { Modal, Image } from '@mantine/core';
+import { getStorage } from 'firebase/storage';
+import { Modal } from '@mantine/core';
+import Stake from '../Teamlogos/StakeSauber.png';
+import RedBullIcon from './../Teamlogos/RedBullIcon.png';
+import FastestLap from './FastestLap.png';
 
 // Flaggen
 import Bahrain from './../Flaggen/bahrain.png';
@@ -47,9 +50,10 @@ function TeilnehmerTabelle() {
     const [userID, setUserID] = useState('');
     const navigate = useNavigate();
     const storage = getStorage();
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [clickedFlag, setClickedFlag] = useState(null);
+    const [selectedFlag, setSelectedFlag] = useState(false);
     const [Strecken, setStrecken] = useState([]);
+    const [nameClickedFlag, setNameClickedFlag] = useState(null);
+    const [RennInformationen, setRennInformationen] = useState([]);
 
     useEffect(() => {
         const personenRef = collection(db, 'personen');
@@ -93,10 +97,6 @@ function TeilnehmerTabelle() {
         // Aufräumen bei Unmount
         return () => unsubscribe();
     }, []);
-
-    useEffect(() => {
-        console.log("Bilder: ", selectedImage);
-    }, [selectedImage]);
 
     const handleClick = (id) => {
         navigate(`/profil/${id}`);
@@ -152,25 +152,108 @@ function TeilnehmerTabelle() {
         return { backgroundColor: 'transparent' };
     }
 
-   function modalFunction(strecke) {
+    async function modalFunction(strecke) {
         console.log("Streckenname:", strecke.name);
-        const listRef = ref(storage, 'gs://f1-liga-int-legendz.appspot.com');
-        listAll(listRef)
-            .then((res) => {
-                const promises = res.items
-                    .filter(itemRef => itemRef.name.includes(strecke.name))
-                    .map(itemRef => getDownloadURL(itemRef));
+        const docRef = doc(db, 'Ergebnisse', strecke.name);
+        const docSnap = await getDoc(docRef);
 
-                return Promise.all(promises);
-            })
-            .then((urls) => {
-                setSelectedImage(urls);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        setClickedFlag(strecke.name);
+        if (docSnap.exists()) {
+            setRennInformationen(docSnap.data());
+            setSelectedFlag(true);
+        } else {
+            console.log("Kein solches Dokument gefunden!");
+        }
     };
+
+    function formatRaceTime(timeInSeconds) {
+        const minutes = Math.floor(timeInSeconds / 60);
+        const seconds = Math.floor(timeInSeconds % 60).toString().padStart(2, '0');
+        const milliseconds = Math.floor((timeInSeconds * 1000) % 1000).toString().padStart(3, '0');
+
+        return `${minutes}:${seconds}:${milliseconds}`;
+    }
+
+    function formatBestRaceTime(timeInMilliseconds) {
+        const minutes = Math.floor(timeInMilliseconds / 60000);
+        const seconds = Math.floor((timeInMilliseconds % 60000) / 1000).toString().padStart(2, '0');
+        const milliseconds = (timeInMilliseconds % 1000).toString().padStart(3, '0');
+
+        return `${minutes}:${seconds}:${milliseconds}`;
+    }
+
+    function formatResultStatus(status) {
+        const statuses = {
+            0: 'invcalid',
+            1: 'inactive',
+            2: 'active',
+            3: 'finished',
+            4: 'DNF',
+            5: 'Disqualifziert',
+            6: 'Nicht gestartet',
+            7: 'Zurückgezogen',
+        }
+
+        return statuses[status] || 'Status unbekannt';
+
+    }
+
+    function getNationality(nationalityId) {
+        const nationalities = {
+            1: 'https://cdn.countryflags.com/thumbs/united-states-of-america/flag-square-250.png',
+            3: 'https://cdn.countryflags.com/thumbs/australia/flag-square-250.png',
+            4: 'https://cdn.countryflags.com/thumbs/austria/flag-square-250.png',
+            9: 'https://cdn.countryflags.com/thumbs/brazil/flag-square-250.png',
+            10: 'https://cdn.countryflags.com/thumbs/united-kingdom/flag-square-250.png',
+            13: 'https://cdn.countryflags.com/thumbs/canada/flag-square-250.png',
+            15: 'https://cdn.countryflags.com/thumbs/china/flag-square-250.png',
+            21: 'https://cdn.countryflags.com/thumbs/denmark/flag-square-250.png',
+            22: 'https://cdn.countryflags.com/thumbs/netherlands/flag-square-250.png',
+            24: 'https://cdn.countryflags.com/thumbs/england/flag-square-250.png',
+            27: 'https://cdn.countryflags.com/thumbs/finland/flag-square-250.png',
+            28: 'https://cdn.countryflags.com/thumbs/france/flag-square-250.png',
+            29: 'https://cdn.countryflags.com/thumbs/germany/flag-square-250.png',
+            31: 'https://cdn.countryflags.com/thumbs/greece/flag-square-250.png',
+            40: 'https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png',
+            41: 'https://cdn.countryflags.com/thumbs/italy/flag-square-250.png',
+            42: 'https://cdn.countryflags.com/thumbs/jamaica/flag-square-500.png',
+            43: 'https://cdn.countryflags.com/thumbs/japan/flag-square-250.png',
+            49: 'https://cdn.countryflags.com/thumbs/luxembourg/flag-square-500.png',
+            52: 'https://cdn.countryflags.com/thumbs/mexico/flag-square-250.png',
+            53: 'https://cdn.countryflags.com/thumbs/monaco/flag-square-250.png',
+            64: 'https://cdn.countryflags.com/thumbs/portugal/flag-square-500.png',
+            77: 'https://cdn.countryflags.com/thumbs/spain/flag-square-250.png',
+            80: 'https://cdn.countryflags.com/thumbs/thailand/flag-square-250.png',
+            89: 'https://cdn.countryflags.com/thumbs/bosnia-and-herzegovina/flag-square-250.png'
+
+        };
+
+        return nationalities[nationalityId] || '/pfad/zum/standardbild.png';
+    }
+
+    function getTeam(teamId) {
+        const teams = {
+            0: { url: 'https://cdn3.emoji.gg/emojis/6785_Mercedes_Logo.png', width: '25', height: '25' }, // Mercedes
+            1: { url: 'https://cdn3.emoji.gg/emojis/ferrari.png', width: '22', height: '28' }, // Ferrari
+            2: { url: RedBullIcon, width: '50', height: '35' }, // Red Bull
+            3: { url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Logo_Williams_F1.png/300px-Logo_Williams_F1.png', width: '25', height: '20' }, // Williams
+            4: { url: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Aston_Martin_Lagonda_brand_logo.png/220px-Aston_Martin_Lagonda_brand_logo.png', width: '40', height: '20' }, // Aston Martin
+            5: { url: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Alpine_logo.png', width: '35', height: '25' }, // Alpine
+            6: { url: 'https://www.visacashapprb.com/wp-content/uploads/2024/01/logo-f1-1.svg', width: '30', height: '30' }, // Visa RB
+            7: { url: 'https://logos-world.net/wp-content/uploads/2022/07/Haas-Symbol-700x394.png', width: '45', height: '25' }, // Haas
+            8: { url: 'https://cdn3.emoji.gg/emojis/9807_McLaren_Logo.png', width: '35', height: '35' }, // McLaren
+            9: { url: Stake, width: '30', height: '30' }  // Stake F1
+        };
+
+        const screenWidth = window.innerWidth;
+        if (screenWidth < 700) {
+            Object.keys(teams).forEach(key => {
+                teams[key].width = (teams[key].width * 0.8).toString();
+                teams[key].height = (teams[key].height * 0.8).toString();
+            });
+        }
+
+        return teams[teamId] || { url: '/pfad/zum/standardbild.png', width: '35', height: '20' };
+    }
 
     return (
         <div className='table-container'>
@@ -190,7 +273,7 @@ function TeilnehmerTabelle() {
                                 .filter(strecke => strecke.ansicht)
                                 .map((strecke, index) => 
                                     <th key={index}>
-                                        <img src={strecke.flagge} alt="Flagge" height="10px" width="20px" onClick={() => modalFunction(strecke)}/>
+                                        <img src={strecke.flagge} alt="Flagge" height="10px" width="20px" onClick={() => { modalFunction(strecke); setNameClickedFlag(strecke.name); }} />
                                     </th>
                                 )
                             }
@@ -233,16 +316,71 @@ function TeilnehmerTabelle() {
                         </tbody>
                 </Table>
             </ScrollArea>
-            <Modal opened={selectedImage !== null} onClose={() => setSelectedImage(null)} title={clickedFlag} centered size="lg"  closeOnClickOutside={false}>
-                {
-                    Array.isArray(selectedImage) && selectedImage.length > 1 ? 
-                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
-                        {selectedImage.map((url, index) => (
-                            <Image key={index} src={url} alt="Streckenbild" width="100%" height="100%" style={{ marginTop: '5px' }} />
-                        ))}
-                    </div> :
-                    <Image src={selectedImage} alt="Streckenbild" width="100%" height="100%" />
-                }
+            <Modal opened={selectedFlag} onClose={() => setSelectedFlag(false)} title={nameClickedFlag} size="100%" closeOnClickOutside={false} zIndex={9999}>
+                <ScrollArea w="auto">
+                    <Box w="auto">
+                        <Table striped bordered hover>
+                            <thead id='theadRaceResult'>
+                                <tr style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+                                    <th>Position</th>
+                                    <th style={{ position: 'sticky', left: 0, zIndex: 1, backgroundColor: 'white' }}>Fahrer</th>
+                                    <th>Nationalität</th>
+                                    <th>Team</th>
+                                    <th>Grid Position</th>
+                                    <th>Beste Persönliche Runde</th>
+                                    <th>Rennzeit</th>
+                                    <th>Punkte</th>
+                                    <th>Strafen (s)</th>
+                                    <th>Anzahl der boxenstopps</th>
+                                    <th>Ergebnisstatus</th>
+                                </tr>
+                            </thead>
+                            <tbody id='tbodyRaceResult'>
+                                {
+                                    (() => {
+                                        const teilnehmerMitInfos = Object.keys(RennInformationen)
+                                            .map(key => ({ ...RennInformationen[key], key }));
+                                        const schnellsteRunde = Math.min(...teilnehmerMitInfos.map(info => info.bestePersoenlicheRunde));
+
+                                        return teilnehmerMitInfos
+                                            .sort((a, b) => a.position - b.position)
+                                            .map(({ key, ...info }) => (
+                                                <tr key={key} style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+                                                    <td>{info.position}</td>
+                                                    <td style={{ position: 'sticky', left: 0, backgroundColor: 'white', zIndex: 1 }}>{info.fahrername}</td>
+                                                    <td>
+                                                        <img src={getNationality(info.nationalitaet)} alt="Nationalität" width="15" height="15" />
+                                                    </td>
+                                                    <td>
+                                                        <img 
+                                                            src={getTeam(info.team).url} 
+                                                            alt="Team" 
+                                                            width={getTeam(info.team).width} 
+                                                            height={getTeam(info.team).height} 
+                                                        />
+                                                    </td>
+                                                    <td>{info.gridPosition}</td>
+                                                    <td style={{ color: info.bestePersoenlicheRunde === schnellsteRunde ? 'purple' : 'inherit' }}>
+                                                        {formatBestRaceTime(info.bestePersoenlicheRunde)}
+                                                        {info.bestePersoenlicheRunde === schnellsteRunde && (
+                                                            <img src={FastestLap} alt="Schnellste Runde" width="15" height="15" style={{marginLeft: '5px'}}/>
+                                                        )}
+                                                    </td>
+                                                    <td>{formatRaceTime(info.rennzeit)}</td>
+                                                    <td>{info.punkte}</td>
+                                                    <td>{info.strafen}</td>
+                                                    <td>{info.boxenstopps}</td>
+                                                    <td>{info.ergebnisStatus !== 3 ? 
+                                                        formatResultStatus(info.ergebnisStatus) : ''}
+                                                    </td>
+                                                </tr>
+                                            ));
+                                    })()
+                                }
+                            </tbody>
+                        </Table>
+                    </Box>
+                </ScrollArea>
             </Modal>
         </div>
     );
