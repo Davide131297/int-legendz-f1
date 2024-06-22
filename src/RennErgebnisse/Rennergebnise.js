30142d615c38fcc1d9da207a7eeeab399e9d2355
davidechiffi <davide.chiffi@iu-study.org>
2024-06-22 19:29:57 +0200

    Download JSON

--- src/RennErgebnisse/Rennergebnise.js
+++ src/RennErgebnisse/Rennergebnise.js
@@ -75,6 +75,41 @@ const Rennergebnise = () => {
         });
     }, []);
 
+    const downloadJSON = () => {
+        const data = RennErgebnis.slice(0, -2)
+            .map((ergebnis, index) => ({
+                fahrer: Fahrerliste[index],
+                ergebnis,
+                rundendaten: Rundendaten[index]
+            }))
+            .filter(item => item.ergebnis.m_position !== 0)
+            .sort((a, b) => a.ergebnis.m_position - b.ergebnis.m_position)
+            .map((item) => ({
+                position: item.ergebnis.m_position,
+                nationalitaet: item.fahrer.m_nationality,
+                fahrername: item.fahrer.m_name,
+                team: item.fahrer.m_teamId,
+                gridPosition: item.ergebnis.m_gridPosition,
+                bestePersoenlicheRunde: item.ergebnis.m_bestLapTimeInMS,
+                rennzeit: item.ergebnis.m_totalRaceTime,
+                punkte: item.ergebnis.m_points,
+                ergebnisStatus: item.ergebnis.m_resultStatus,
+                boxenstopps: item.rundendaten ? item.rundendaten.m_numPitStops : 'N/A',
+                strafen: item.rundendaten ? item.rundendaten.m_penalties : 'N/A'
+            }));
+
+        const json = JSON.stringify(data, null, 2);
+        const blob = new Blob([json], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'Rennergebnis.json';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
 
     return (
         <>
@@ -107,7 +142,7 @@ const Rennergebnise = () => {
                 </Tabs.Panel>
 
                 <Tabs.Panel value="settings">
-                    Hier wird noch etwas neues entstehen...
+                    <button onClick={downloadJSON}>Download JSON</button>
                 </Tabs.Panel>
             </Tabs>
         </div>
